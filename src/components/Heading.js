18c76a4be2270164a6fe81c8221825e81e3a0d35
Angelo Ashmore <angelo.ashmore@walltowall.com>
2019-11-01 17:40:27 -1000

    feat: add alt styling

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -2,4 +2,11 @@ import React from 'react'
 
 import { Heading as HeadingBase } from 'system'
 
-export const Heading = props => <HeadingBase fontWeight="bold" {...props} />
+export const Heading = props => (
+  <HeadingBase
+    fontWeight="black"
+    fontSizeScale="l"
+    lineHeight="title"
+    {...props}
+  />
+)
--- src/slices/PageBodyFullViewportImage.js
+++ src/slices/PageBodyFullViewportImage.js
@@ -1,30 +1,116 @@
 import React from 'react'
 import { graphql } from 'gatsby'
-import { getImageFluid } from 'helpers'
+import { camelCase } from 'tiny-compose-fns'
+import { notEmpty, getRichText, getImageFluid } from 'helpers'
 
-import { ImageContainer, Image } from 'system'
+import { safeHexToP3 } from 'src/helpers'
+
+import { Box, Flex, ImageContainer, Image, Link } from 'system'
+import { BoundedBox, Button, HTMLContent } from 'src/components'
+
+const normalizedTextVerticalAlignments = {
+  center: 'center',
+  top: 'flex-start',
+  bottom: 'flex-end',
+}
 
 export const PageBodyFullViewportImage = ({
+  textColor = 'inherit',
+  buttonColor = 'black',
+  buttonTextColor = 'white',
+  fontFamily = 'inherit',
   imageFluid,
   imageURL,
   imageAlt,
+  textHTML,
+  textVerticalAlignment = 'center',
+  buttons = [],
   ...props
-}) => (
-  <ImageContainer height="100vh">
-    <Image
-      fluid={imageFluid}
-      src={imageURL}
-      alt={imageAlt}
+}) => {
+  const normalizedTextVerticalAlignment =
+    normalizedTextVerticalAlignments[textVerticalAlignment]
+
+  const textColorP3 = safeHexToP3(textColor)
+  const buttonColorP3 = safeHexToP3(buttonColor)
+  const buttonTextColorP3 = safeHexToP3(buttonTextColor)
+
+  return (
+    <BoundedBox
+      position="relative"
+      height="100vh"
+      pt="20vh"
+      pb="20vh"
       {...props}
-      css={{ userSelect: 'none', pointerEvents: 'none' }}
-    />
-  </ImageContainer>
-)
+    >
+      <ImageContainer
+        position="absolute"
+        top={0}
+        left={0}
+        right={0}
+        bottom={0}
+        css={{ pointerEvents: 'none', userSelect: 'none' }}
+      >
+        <Image
+          fluid={imageFluid}
+          src={imageURL}
+          alt={imageAlt}
+          {...props}
+          css={{ userSelect: 'none', pointerEvents: 'none' }}
+        />
+      </ImageContainer>
+      <Flex height="100%" justifyContent="center">
+        <Flex
+          position="relative"
+          flexDirection="column"
+          alignSelf={normalizedTextVerticalAlignment}
+          maxWidth="m"
+        >
+          <HTMLContent
+            html={textHTML}
+            color={textColorP3}
+            fontFamily={fontFamily}
+            fontSizeScale="m"
+            mbScale="m-"
+            textAlign="center"
+            fontWeight="bold"
+          />
+          <Flex as="ul" flexWrap="wrap" justifyContent="center">
+            {buttons.map(button => (
+              <Box key={button?.href} as="li" mrScale="s">
+                <Button
+                  as={Link}
+                  href={button?.href}
+                  bg={buttonColorP3}
+                  color={buttonTextColorP3}
+                >
+                  {button?.text}
+                </Button>
+              </Box>
+            ))}
+          </Flex>
+        </Flex>
+      </Flex>
+    </BoundedBox>
+  )
+}
 
 PageBodyFullViewportImage.mapDataToProps = ({ data }) => ({
+  textColor: data?.primary?.text_color,
+  buttonColor: data?.primary?.button_color,
+  buttonTextColor: data?.primary?.button_text_color,
+  fontFamily: data?.primary?.font_family?.text,
   imageFluid: getImageFluid(data?.primary?.image),
   imageURL: data?.primary?.image?.url,
   imageAlt: data?.primary?.image?.alt,
+  textHTML: getRichText(data?.primary?.text),
+  textVerticalAlignment: notEmpty(
+    camelCase(data?.primary?.text_vertical_alignment),
+  ),
+  buttons: data?.items?.map?.(item => ({
+    text: item?.button_text?.text,
+    href: item?.button_link?.url,
+    target: item?.button_link?.target,
+  })),
 })
 
 export const fragment = graphql`
@@ -35,6 +121,17 @@ export const fragment = graphql`
           ... on PrismicPageBodyFullViewportImage {
             id
             primary {
+              text_color
+              button_color
+              button_text_color
+              font_family {
+                text
+              }
+              text {
+                text
+                html
+              }
+              text_vertical_alignment
               image {
                 alt
                 localFile {
@@ -49,6 +146,15 @@ export const fragment = graphql`
                     }
                   }
                 }
+              }
+            }
+            items {
+              button_text {
+                text
+              }
+              button_link {
+                url
+                target
               }
             }
           }
