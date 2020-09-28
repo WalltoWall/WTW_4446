5095d87702b0e23ba1ed6d744e2779f9ca369b96
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2020-09-28 13:09:26 -1000

    fix: judge button text color

--- src/components/Judge.js
+++ src/components/Judge.js
@@ -95,7 +95,7 @@ export const Judge = ({
                   width={linearScale('12px', '30px', { count: 5 })}
                 />
               </ButtonCircle>
-              <Subheading as="span" fontSizeScale="m" textStyle="caps">
+              <Subheading as="span" fontSizeScale="m" textStyle="caps" color="buttonBackground">
                 Bio
               </Subheading>
             </Flex>
@@ -110,78 +110,6 @@ export const Judge = ({
           </Box>
         )}
       </Box>
-    </Grid>
-  )
-
-  return (
-    <Grid
-      bg="background"
-      gridAutoFlow="dense"
-      gridTemplateColumns={['1fr', 'repeat(2, 1fr)']}
-      gridRowGapScale="m"
-      gridColumnGapScale="xxl"
-      alignItems="center"
-      {...props}
-    >
-      <Flex
-        justifyContent={['center', imageIsOnLeft ? 'flex-end' : 'flex-start']}
-        gridColumn={[null, imageIsOnLeft ? 1 : 2]}
-      >
-        <ImageContainer maxWidth={['30rem', 'none']} width="100%">
-          <AspectRatio x={6} y={7}>
-            <Image fluid={imageFluid} src={imageURL} alt={imageAlt} />
-          </AspectRatio>
-        </ImageContainer>
-      </Flex>
-      <Flex
-        justifyContent={['center', imageIsOnLeft ? 'flex-start' : 'flex-end']}
-        gridColumn={[null, imageIsOnLeft ? 2 : 1]}
-      >
-        <Box width="100%" maxWidth={['30rem', '30ch']}>
-          {name && (
-            <Heading fontSizeScale="xl" mbScale="t-">
-              {name}
-            </Heading>
-          )}
-          {jobTitle && (
-            <Text as="p" mbScale="t-">
-              {jobTitle}
-            </Text>
-          )}
-          {location && (
-            <Subheading as="p" color="body" mbScale="t-">
-              {location}
-            </Subheading>
-          )}
-          {bioHTML && (
-            <Box mtScale="s">
-              <Flex alignItems="center">
-                <ButtonCircle
-                  height={linearScale('30px', '70px', { count: 5 })}
-                  width={linearScale('30px', '70px', { count: 5 })}
-                  mrScale="t"
-                  onClick={toggleBioIsOpen}
-                >
-                  <SVG
-                    svg={svg.svg}
-                    x={svg.x}
-                    y={svg.y}
-                    width={linearScale('12px', '30px', { count: 5 })}
-                  />
-                </ButtonCircle>
-                <Subheading as="span" fontSizeScale="m" textStyle="caps">
-                  Bio
-                </Subheading>
-              </Flex>
-              <HTMLContent
-                mtScale="t"
-                html={bioHTML}
-                display={bioIsOpen ? 'block' : 'none'}
-              />
-            </Box>
-          )}
-        </Box>
-      </Flex>
     </Grid>
   )
 }
