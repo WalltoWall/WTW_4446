290bf490a6c9e3c77ba4cf9cf04fc81fbd928e3f
Anthony Yarbrough <asyarb@hawaii.edu>
2020-11-02 16:15:16 -1000

    feat: reduce x padding on header footer at wide

--- src/slices/PageBodyHeader.js
+++ src/slices/PageBodyHeader.js
@@ -69,6 +69,7 @@ export const PageBodyHeader = ({
           pyScale="s"
           position="relative"
           zIndex="headerBar"
+          pxScale="l"
           {...props}
         >
           <Grid
